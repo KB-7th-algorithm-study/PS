e8f5467710d534c40f24ad5773782363ed471caf
sanghak <sanghk32@gmail.com>
2026-04-20 23:01:34 +0900

    [1932] 2xn 타일링

--- sanghak/BOJ_11726.java
+++ sanghak/BOJ_11726.java
@@ -1,4 +1,22 @@
 package PS.sanghak;
+import java.util.*;
+import java.lang.*;
+import java.io.*;
 
 public class BOJ_11726 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int[] dp = new int[n+2]; // 안전하게 n+2 크기
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for (int i = 3; i <= n; i++) {
+            dp[i] = (dp[i-1] + dp[i-2]) % 10007;
+        }
+
+        System.out.println(dp[n]);
+    }
 }
+
