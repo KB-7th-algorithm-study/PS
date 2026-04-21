a13b9a6f9a4d8a2490347f8a82aa3792327c25f0
im-minji <mjlim514@gmail.com>
2026-04-21 14:22:12 +0900

    [11726] 2xN 타일링

--- minji/week3/tiling2xn_11726.java
+++ minji/week3/tiling2xn_11726.java
@@ -1,4 +1,33 @@
 package PS.minji.week3;
 
+import java.io.*;
+
 public class tiling2xn_11726 {
+        public static void main(String[] args) throws IOException {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+            // 직사각형 크기 입력 받기
+            int n = Integer.parseInt(br.readLine());
+
+            if (n == 1) {
+                bw.write("1\n");
+                bw.flush();
+                bw.close();
+                return;
+            }
+
+            int[] dp = new int[n+1];
+
+            dp[1] = 1;
+            dp[2] = 2;
+
+            for(int i=3; i<dp.length; i++) {
+                dp[i] = (dp[i-1] + dp[i-2]) % 10007;
+            }
+
+            bw.write(String.valueOf(dp[n]));
+            bw.flush();
+            bw.close();
+        }
 }
