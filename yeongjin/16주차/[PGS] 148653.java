0c9b8dd5db829e372818e488508eeb9ccae38a68
itleo29 <itlei2966@gmail.com>
2026-09-16 14:33:59 +0900

    [PSG] 마법의 엘리베이터 (#94)

--- yeongjin/16주차/[PGS] 148653.java
+++ yeongjin/16주차/[PGS] 148653.java
@@ -3,17 +3,34 @@ class Solution {
     public int solution(int storey) {
         int answer = 0;
         
-        String s = Integer.toString(storey);
-        int n = s.length();
-    
-        for(int i =0; i<n; i++){
-            int a =s.charAt(i) - '0';
-            if(a <=5){
-                answer +=a;
+        while(storey/10 != 0){
+            int n = storey%10;
+            if(n <5){
+                storey -=n;
+                answer +=n;
+            }else if(n >5){
+                storey +=10-n;
+                answer +=10-n;
             }else{
-                answer += 10-a +1;
+                storey /= 10;
+                n = storey%10;
+                if(n >=5){
+                    storey +=1;
+                    
+                }
+                answer +=5;
+                
+                continue;
             }
+            storey /=10;
         }
+        
+        if(storey <=5){
+            answer += storey;
+        }else {
+            answer += 10-storey+1;
+        }
+
         
         return answer;
     }
